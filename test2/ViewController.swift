626e700bcbae9554fa015e3ca36d77de4f6cb09d
MLDLab <eunmi.kim@eku.edu>
2026-01-20 11:58:33 -0500

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Kim, Eunmi on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Initial Commit")
+    }
+
+
+}
+
